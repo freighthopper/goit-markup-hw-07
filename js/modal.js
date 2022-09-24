88a27f0918363d170273f22f44b4391611bba3c2
Rodion Shkondin <trainhoppe@gmail.com>
2022-09-24 03:07:29 +0200

    BEM, review

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,12 @@
 (() => {
     const refs = {
         openModalBtn: document.querySelector("[data-modal-open]"),
-        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal__closeModalBtn: document.querySelector("[data-modal-modal__close]"),
         modal: document.querySelector("[data-modal]"),
     };
 
     refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.modal__closeModalBtn.addEventListener("click", toggleModal);
 
     function toggleModal() {
         refs.modal.classList.toggle("is-hidden");
